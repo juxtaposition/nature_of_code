bf9b7224423747797001f944dd641251529f3dbb
Ezequiel <exbios@about.me>
2017-07-31 22:17:41 -0500

    Ventan para Randon Walkers en GoLang

---
+++ Introduction/GoLang/Exercise1_1/Exercise1_1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+	)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Randon Walkers",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.Clear(colornames.Darkgray)
+
+	for !win.Closed() {
+		win.Update()
+	}
+
+	
+}
+
+func main() {
+	pixelgl.Run(run)
+}
